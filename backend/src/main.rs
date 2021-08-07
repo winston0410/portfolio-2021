966b911d0e168b60707b93cc844f60904a378f6b
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-08-07 19:36:45 +0100

    Move route setup code to router

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -10,11 +10,9 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    routes::tools::setup();
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(manual_hello))
-    })
+    let app = App::new();
+    routes::tools::setup(&app);
+    HttpServer::new( || { &app })
     .bind("0.0.0.0:8080")?
     .run()
     .await
