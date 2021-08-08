bbbecc4e95870e2202c8421890ebe0296e75ac9f
kghugo <hugosum.dev@protonmail.com>
2021-08-08 10:08:55 +0100

    Fix build

--- backend/src/routes/tools.rs
+++ backend/src/routes/tools.rs
@@ -1,12 +1,5 @@
 use crate::types::response;
 
-#[get("/")]
-async fn get() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-pub fn setup(app: actix_web::App){
-    // response::success();
-    println!("hello world");
-    app.service(get)
+pub fn setup(){
+    
 }
