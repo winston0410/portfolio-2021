a3b1a5ef791c7149e25c2a9f3ca21e0457ec000b
kghugo <hugosum.dev@protonmail.com>
2021-08-13 21:31:51 +0100

    Update package

---
+++ web/src/routes/opensource/index.svelte
@@ -0,0 +1,14 @@
+<script context="module" lang="ts">
+  export async function load({ fetch }) {
+    const res = await fetch('/api/github');
+    console.log(res);
+  }
+</script>
+
+<svelte:head>
+	<title>Opensource</title>
+</svelte:head>
+
+<main>
+<h1>Opensource</h1>
+</main>
