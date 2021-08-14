8708fd00fd51c73bd990114a2a920a1e2b08f0ab
kghugo <hugosum.dev@protonmail.com>
2021-08-14 18:21:00 +0100

    Create endpoint for fetching frontend state

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,12 +1,24 @@
-<script lang="ts">
-    import "/src/app.css"
+<script context="module" lang="ts">
+  import "/src/app.css"
 
+  import { getProps } from "/src/helper"
   import glory from "/src/cssRenderer"
 
   const style = glory.virtual({
     fontFamily: "VT323",
     fontSize: "40px",
   })
+
+  import NavBar from "/src/components/NavBar.svelte"
+  
+  export const load = getProps({ pages: '/api/states/nav-bar'});
+</script>
+
+<script lang="ts">
+  export let pages;
+  console.log(pages)
 </script>
 
 <h1 class={style}>Hello</h1>
+
+<!--  <NavBar pages={pages}/>  -->
