16ff2806aded2ac63a4486faa004c66d45999321
kghugo <hugosum.dev@protonmail.com>
2021-08-14 21:28:36 +0100

    Add custom 404 page

---
+++ web/src/routes/__error.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+	export function load({ error, status }) {
+return {
+	props: {
+		title: `${status}: ${error.message}`
+	}
+};
+	}
+</script>
+
+<script>
+	export let title;
+</script>
+
+<h1>Cache me if you can...But this is a 404!</h1>
