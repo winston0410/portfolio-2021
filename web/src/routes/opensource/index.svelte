b66d268ef3f48eac5af7f110d4666d1730658a4e
kghugo <hugosum.dev@protonmail.com>
2021-08-15 22:39:31 +0100

    Fix format for github api

--- web/src/routes/opensource/index.svelte
+++ web/src/routes/opensource/index.svelte
@@ -1,22 +1,13 @@
 <script context="module" lang="ts">
 import { getProps } from '/src/helper'
- //  console.log('check get Props', getProps);
- //  export async function load({ fetch }) {
-    //  const res = await fetch('/api/github');
-  //  if (res.ok) return { props: { projects: await res.json() } };
-  //  console.log('error found', res.status);
-  //  return {
-    //  status: res.status,
-    //  error: new Error()
-   //  };
-  //  }
+
 export const load = getProps({ projects: '/api/github'});
 
 </script>
 
 <script lang="ts">
   export let projects;
-  console.log('check final value', projects);
+  export const { ok, value } = projects;
 </script>
 
 <svelte:head>
@@ -25,5 +16,16 @@ export const load = getProps({ projects: '/api/github'});
 
 <main>
 <h1>Opensource</h1>
+    <ul>
+    <!--  https://docs.github.com/en/rest/reference/repos#list-repository-languages  -->
+	{#each value as { name, description, html_url, language }}
+		<li>
+        <a href={html_url}>
+            <span>{name}</span>
+		</a>
+        <p>{description}</p>
+        </li>
+	{/each}
+    </ul>
 </main>
 
