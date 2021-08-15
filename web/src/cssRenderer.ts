f56f89a5004051336de2cd6884e96513e15839f4
kghugo <hugosum.dev@protonmail.com>
2021-08-15 08:46:30 +0100

    Add hydrate for glory

--- web/src/cssRenderer.ts
+++ web/src/cssRenderer.ts
@@ -1,9 +1,10 @@
-import { create, prefixer, virtual, keyframes } from "glory"
+import { create, prefixer, virtual, keyframes, hydrate } from "glory"
 
 const glory = create({})
 
 virtual(glory);
 prefixer(glory)
 keyframes(glory)
+hydrate(glory)
 
 export default glory
