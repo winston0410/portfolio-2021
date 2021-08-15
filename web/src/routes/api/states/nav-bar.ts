f2566acf2ed58fe97c34ea74f56ac8c057d3a91c
kghugo <hugosum.dev@protonmail.com>
2021-08-15 13:36:33 +0100

    Fix response for nav-bar states

--- web/src/routes/api/states/nav-bar.ts
+++ web/src/routes/api/states/nav-bar.ts
@@ -2,10 +2,8 @@ import type { EndpointOutput } from '@sveltejs/kit'
 
 const pages = [{ name: "opensource", url: "/opensource"}, {name: "My curriculum vitae", url: "/cv"}];
 
-export async function get () :Promise<EndpointOutput<{ pages: Array<{ name: string, url: string}> }>>{
+export async function get () :Promise<EndpointOutput<Array<{ name: string, url: string}>>>{
     return {
-        body: {
-            pages: pages
-        }
+        body: pages
     }
 }
