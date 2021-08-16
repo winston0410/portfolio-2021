2f62080150d14b908b3416a0b7e618c4e5bab179
kghugo <hugosum.dev@protonmail.com>
2021-08-16 21:13:55 +0100

    Use PostCSS

--- shell.nix
+++ shell.nix
@@ -1,3 +1,3 @@
 { pkgs ? import <nixpkgs> { } }:
 
-pkgs.mkShell { buildInputs = with pkgs; [ elixir_1_11 nodejs-16_x cargo rustc]; }
+pkgs.mkShell { buildInputs = with pkgs; [ nodejs-16_x cargo rustc git-town]; }
