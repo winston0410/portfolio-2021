d65cce70b108b7503bc616759158be5b4faa8b67
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-08-27 07:31:27 +0100

    Change format for fetch langauges from github

--- web/src/routes/api/github.ts
+++ web/src/routes/api/github.ts
@@ -5,7 +5,7 @@ import env from '../../env'
 const getGithubApi = async () => {
 	return fetch('https://api.github.com/users/winston0410/repos?sort=updated&per_page=100', {
         headers: {
-            Authorization: 'Basic ' + Buffer.from(`winston0410:${env.VITE_GITHUB_TOKEN}`).toString('base64');
+            Authorization: 'Basic ' + Buffer.from(`winston0410:${env.VITE_GITHUB_TOKEN}`).toString('base64')
         }
     }).then(
 		(res) => res.json()
@@ -19,14 +19,16 @@ const getGithubApi = async () => {
 export const get: RequestHandler<Locals> = async () => {
 	try {
 		const res = await getGithubApi();
-		const fetchList = [];
 		for (const repo of res) {
-			fetchList.push(fetch(repo.languages_url));
+            repo.languages = await fetch(repo.languages_url)
+            //  seems like overlimit is not 400
+            .then(res => res.json())
+            .catch(err => {
+                console.log(err)
+                return {}
+            })
+			//  fetchList.push(fetch(repo.languages_url));
 		}
-
-		const languages = await Promise.all(fetchList.map((p) => p.then(res => res.json()).catch(e => e)))
-        
-		console.log('check langagues', languages);
 
 		return {
 			body: res.filter((item) => !item.fork && !item.archived && !item.disabled && item.description)
