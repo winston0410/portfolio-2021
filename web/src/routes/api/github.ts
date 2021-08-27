85eb58576e30f2fb1b895ace17916ebb6ba9d7ed
kghugo <hugosum.dev@protonmail.com>
2021-08-27 10:40:38 +0100

    Parse markdown to emoji

--- web/src/routes/api/github.ts
+++ web/src/routes/api/github.ts
@@ -1,48 +1,71 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import type { Locals } from '$lib/types';
-import env from '../../env'
+import env from '../../env';
+import MarkDownIt from 'markdown-it';
+import emoji from 'markdown-it-emoji';
 
-const fetchOptions = {
-        headers: {
-            Authorization: 'Basic ' + Buffer.from(`winston0410:${env.VITE_GITHUB_TOKEN}`).toString('base64')
-        }
-}
-
-const getGithubApi = async () => {
-	return fetch('https://api.github.com/users/winston0410/repos?sort=updated&per_page=100', fetchOptions).then(
-		(res) => res.json()
-	);
+const processMarkdown = (text: string): string => {
+    const md = new MarkDownIt()
+    md.use(emoji, []);
+	return md.renderInline(text);
 };
 
-//  interface IGithubRepo {
-    
-//  }
+const fetchOptions = {
+	headers: {
+		Authorization: 'Basic ' + Buffer.from(`winston0410:${env.VITE_GITHUB_TOKEN}`).toString('base64')
+	}
+};
+
+const getGithubApi = async () => {
+	return fetch(
+		'https://api.github.com/users/winston0410/repos?sort=updated&per_page=100',
+		fetchOptions
+	).then((res) => res.json());
+};
+
+interface IGithubRepo {
+	name: string;
+	description: string;
+	html_url: string;
+	languages_url: string;
+	fork: boolean;
+	archived: boolean;
+	disabled: boolean;
+}
 
 export const get: RequestHandler<Locals> = async () => {
 	try {
 		const githubRes = await getGithubApi();
-        const filtered = githubRes.filter((item) => !item.fork && !item.archived && !item.disabled && item.description)
-        
+		const filtered = githubRes
+			.filter(
+				(item: IGithubRepo) => !item.fork && !item.archived && !item.disabled && item.description
+			)
+			.map((item: IGithubRepo) => {
+				item.description = processMarkdown(item.description);
+                console.log('check value', item.description)
+				return item;
+			});
+
 		for (const repo of filtered) {
-            repo.languages = await fetch(repo.languages_url, fetchOptions)
-            .then(res => {
-                if (!res.ok) {
-                    throw Error(res.statusText)
-                }
-                return res
-            })
-            .then(res => res.json())
-            .catch(err => {
-                return {}
-            })
+			repo.languages = await fetch(repo.languages_url, fetchOptions)
+				.then((res) => {
+					if (!res.ok) {
+						throw Error(res.statusText);
+					}
+					return res;
+				})
+				.then((res) => res.json())
+				.catch((err) => {
+					return {};
+				});
 		}
 
 		return {
 			body: filtered
 		};
 	} catch (error) {
-      return {
-          body: error
-      }
-    }
-}
+		return {
+			body: error
+		};
+	}
+};
