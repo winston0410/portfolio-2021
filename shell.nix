cb3bb9bbeb32b47e805e7ced181fde039262e341
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-08-30 22:18:18 +0100

    Try using single font

--- shell.nix
+++ shell.nix
@@ -1,3 +1,3 @@
 { pkgs ? import <nixpkgs> { } }:
 
-pkgs.mkShell { buildInputs = with pkgs; [ nodejs-16_x cargo rustc git-town nodePackages.pnpm]; }
+pkgs.mkShell { buildInputs = with pkgs; [ nodejs-16_x cargo rustc git-town nodePackages.pnpm batik]; }
