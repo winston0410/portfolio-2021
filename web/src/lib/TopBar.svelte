1e018e60952eb10dcb1aa8b2a2b4e4ba1dc48470
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-08-31 16:01:36 +0100

    Extract topbar related code

---
+++ web/src/lib/TopBar.svelte
@@ -0,0 +1,22 @@
+<style>
+    .topbar {
+      position: fixed;
+      top: 0px;
+      left: 0px;
+      right: 0px;
+      color: var(--base-contrast-color);
+      background: var(--base-color);
+      padding: var(--md-space) 0;
+    }
+
+    .topbar-inner{
+        display: flex;
+        justify-content: center;
+     }
+</style>
+
+<header class="topbar">
+    <div class="topbar-inner">
+    This is the top bar
+    </div>
+</header>
--- web/src/routes/__layout.svelte
+++ web/src/routes/__layout.svelte
@@ -61,21 +61,6 @@ socialProfiles.set(_socialProfiles.value)
         color: #000;
     }
 
-    .topbar {
-      position: fixed;
-      top: 0px;
-      left: 0px;
-      right: 0px;
-      color: var(--base-contrast-color);
-      background: var(--base-color);
-      padding: var(--md-space) 0;
-    }
-
-    .topbar-inner{
-        display: flex;
-        justify-content: center;
-     }
-
     .main{
       color: var(--base-contrast-color);
       background: var(--base-color);
@@ -85,11 +70,6 @@ socialProfiles.set(_socialProfiles.value)
     }
 </style>
 
-<!--  <header class="topbar">  -->
-    <!--  <div class="topbar-inner">  -->
-    <!--  This is the top bar  -->
-    <!--  </div>  -->
-<!--  </header>  -->
 <div class="main">
     <slot></slot>
 </div>
