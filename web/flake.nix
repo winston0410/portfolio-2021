5417763c7cb91aa0b138136829e2d5912a9b0949
kghugo <hugosum.dev@protonmail.com>
2021-10-09 15:46:29 +0100

    Create derivation for the project

---
+++ web/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Flake for my portfolio";
+
+  inputs = {
+    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    mkNodePackage = { url = "github:winston0410/mkNodePackage/develop"; };
+  };
+
+  outputs = { nixpkgs, mkNodePackage, ... }:
+    let system = "x86_64-linux";
+    in {
+      defaultPackage.${system} = (mkNodePackage.lib.${system}.mkNodePackage {
+        pname = "hugo-porfolio";
+        version = "1.0.0";
+        src = ./.;
+
+        buildPhase = ''
+          npm run build
+        '';
+
+        installPhase = ''
+          mkdir -p $out
+          cp -r build $out 
+        '';
+      });
+      devShell.${system} = (({ pkgs, ... }:
+        pkgs.mkShell {
+          buildInputs = with pkgs; [ nodejs ];
+
+          shellHook = "";
+        }) { pkgs = nixpkgs.legacyPackages.${system}; });
+    };
+}
