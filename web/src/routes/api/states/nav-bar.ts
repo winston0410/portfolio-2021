a7a703ecd98adb0812e95e117f39a1a6624b536b
kghugo <hugosum.dev@protonmail.com>
2021-11-17 17:52:42 +0000

    Remove blog related features for now

--- web/src/routes/api/states/nav-bar.ts
+++ web/src/routes/api/states/nav-bar.ts
@@ -4,7 +4,7 @@ import type { INavItem } from '$lib/typing';
 const pages = [
 	{ name: 'Opensource', url: '/pages/opensource' },
 	{ name: 'My curriculum vitae', url: '/pages/cv' },
-	{ name: 'Blog', url: '/blog' }
+	//  { name: 'Blog', url: '/blog' }
 ];
 
 export async function get(): Promise<EndpointOutput<Array<INavItem>>> {
