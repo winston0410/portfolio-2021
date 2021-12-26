bb8dba46fbe3b8996d4e936be8774f6407041855
kghugo <hugosum.dev@protonmail.com>
2021-12-26 16:56:07 +0000

    Try chrome playwright

--- web/pdf.ts
+++ web/pdf.ts
@@ -1,13 +1,19 @@
 // NOTE Need to use serverless version as Vercel doesn't support custom binaries
 //  import playwright from 'playwright';
-import playwright from 'playwright-aws-lambda';
+//  import playwright from 'playwright-aws-lambda';
+import playwright from 'playwright-core';
+import chromium from 'chrome-aws-lambda';
 import fs from 'fs';
 
 (async function () {
 	const resumePage = fs.readFileSync('./build/pages/resume/index.html', 'utf8');
 
-	//  const browser = await playwright.chromium.launch();
-	const browser = await playwright.launchChromium();
+	const browser = await playwright.chromium.launch({
+		args: chromium.args,
+		executablePath: await chromium.executablePath,
+		headless: chromium.headless
+	});
+
 	const context = await browser.newContext();
 	const page = await context.newPage();
 	await page.setContent(resumePage);
