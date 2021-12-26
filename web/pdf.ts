7f62a0d46b6dfd0ee6ea3be3c2431d5bb5d5a1d0
kghugo <hugosum.dev@protonmail.com>
2021-12-26 16:20:35 +0000

    Use aws playwright

--- web/pdf.ts
+++ web/pdf.ts
@@ -1,20 +1,23 @@
-import playwright from 'playwright';
+// NOTE Need to use serverless version as Vercel doesn't support custom binaries
+//  import playwright from 'playwright';
+import playwright from 'playwright-aws-lambda';
 import fs from 'fs';
 
 (async function () {
 	const resumePage = fs.readFileSync('./build/pages/resume/index.html', 'utf8');
 
-	const browser = await playwright.chromium.launch();
+	//  const browser = await playwright.chromium.launch();
+	const browser = await playwright.launchChromium();
 	const context = await browser.newContext();
 	const page = await context.newPage();
 	await page.setContent(resumePage);
 
 	await page.pdf({
 		path: './build/resume.pdf',
-		format: 'A4',
+		format: 'A4'
 	});
 
-    console.log('PDF generated successfully!')
+	console.log('PDF generated successfully!');
 
 	await browser.close();
 })();
