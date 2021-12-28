d92e69fc2408fb5748ae2a24f76cf9c5db105e9b
kghugo <hugosum.dev@protonmail.com>
2021-12-28 22:35:30 +0000

    Trying to cache request

--- pdf-service/src/routes/resume/index.ts
+++ pdf-service/src/routes/resume/index.ts
@@ -1,16 +1,23 @@
 import playwright from "playwright";
-//  import fs from 'fs'
+import fs from 'fs'
 import fetch from "node-fetch";
-import type { FastifyPluginCallback, FastifyReply, FastifyRequest } from "fastify";
+import type {
+  FastifyPluginCallback,
+  FastifyReply,
+  FastifyRequest,
+} from "fastify";
 
 type IVersion = {
   generated_at: number;
   version: string;
 };
 
+let lastGeneratedAt = -1;
+
 const plugin: FastifyPluginCallback = (server, _, next) => {
   server.get("/", async (req: FastifyRequest, res: FastifyReply) => {
-    const websiteDomain = "http://localhost:3000";
+    const websiteDomain = "https://hugosum.me";
+    //  const websiteDomain = process.env.FRONTEND_URL;
 
     const versionRes = await fetch(websiteDomain + "/api/version");
 
@@ -22,20 +29,29 @@ const plugin: FastifyPluginCallback = (server, _, next) => {
     }
 
     // REF No generic for json() or fetch()
-    const { generated_at } = (await versionRes.json() as IVersion);
+    const { generated_at, version } = (await versionRes.json()) as IVersion;
 
-    console.log("check value", generated_at);
+    console.log('check value', generated_at, lastGeneratedAt)
 
-    const browserType = playwright.chromium;
-    const browser = await browserType.launch();
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto("https://hugosum.me/pages/resume");
-    const pdf = await page.pdf({ format: "A4" });
-    //  await fs.promises.writeFile('./resume-hugosum.pdf', pdf);
-    await browser.close();
+    if (generated_at > lastGeneratedAt) {
+      console.log('first condition')
+      const browserType = playwright.chromium;
+      const browser = await browserType.launch();
+      const context = await browser.newContext();
+      const page = await context.newPage();
+      await page.goto("https://hugosum.me/pages/resume");
+      const pdf = await page.pdf({ format: "A4" });
+      await fs.promises.writeFile('./resume-hugosum.pdf', pdf);
+      await browser.close();
 
-    res.code(200).type("application/pdf").send(pdf);
+      lastGeneratedAt = generated_at
+
+      res.code(200).type("application/pdf").send(pdf);
+    } else {
+      console.log('not bigger than')
+      const pdf = await fs.promises.readFile('./resume-hugosum.pdf')
+      res.code(200).type("application/pdf").send(pdf);
+    }
   });
 
   next();
