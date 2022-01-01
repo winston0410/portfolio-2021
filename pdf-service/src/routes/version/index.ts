cb17ddafb7d5eab8bf69286700f0cb9df56b7837
kghugo <hugosum.dev@protonmail.com>
2022-01-01 13:35:53 +0000

    Use env for build version

--- pdf-service/src/routes/version/index.ts
+++ pdf-service/src/routes/version/index.ts
@@ -3,11 +3,10 @@ import type {
   FastifyReply,
   FastifyRequest,
 } from "fastify";
-import cp from 'child_process';
 
 const plugin: FastifyPluginCallback = (server, _, next) => {
   server.get("/", async (req: FastifyRequest, res: FastifyReply) => {
-	const version = cp.execSync('git rev-parse HEAD').toString().trim().slice(0, 7);
+    const version = process.env.LAST_COMMIT_HASH
 
     res.code(200).send({
         version
