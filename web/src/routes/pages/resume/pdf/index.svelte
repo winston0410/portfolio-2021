028f2a4d1fa67ae2e1bedbb4c26a9c4fff0f3e3c
kghugo <hugosum.dev@protonmail.com>
2022-01-02 17:38:41 +0000

    Use pdf object

---
+++ web/src/routes/pages/resume/pdf/index.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" context="module">
+    import PDFObject from 'pdfobject'
+</script>
+
+<script lang="ts">
+    const title = "Resume | Hugo Sum"
+    const opts = {
+        title,
+        fallbackLink: false
+    }
+    
+    const res = PDFObject.embed("https://pdf.hugosum.me/resume", undefined, opts)
+
+    if(!res){
+        console.log('browser not supported')
+    }
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
