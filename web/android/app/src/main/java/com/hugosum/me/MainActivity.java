b7ea7e245c977351799a1b51ffbd72206c075b86
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2022-05-05 08:46:17 +0100

    Add cap

---
+++ web/android/app/src/main/java/com/hugosum/me/MainActivity.java
@@ -0,0 +1,5 @@
+package com.hugosum.me;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ web/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hugosum.me',
+  appName: 'portfolio-2021',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
